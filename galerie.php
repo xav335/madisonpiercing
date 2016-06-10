49983518f14494159b4e170ac21fc074dea63155
Xavier Gonzalez <xavier@gonzalez.pm>
2016-06-10 12:28:59 +0200

    fix portview

--- galerie.php
+++ galerie.php
@@ -21,8 +21,8 @@
 <html class="no-js" lang="fr">
 	<head>
 		<meta charset="utf-8" />
-		<meta http-equiv="x-ua-compatible" data-fancybox-legende="ie=edge">
-		<meta name="viewport" data-fancybox-legende="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Madison Piercing | Galerie photos</title>
 		
 		<? include( $_SERVER[ "DOCUMENT_ROOT" ] . "/inc/header.php" ); ?>
