33ab3ea201ddbfab94f2764b9b7efdce7ab4c5b9
Javier <jgonzalez@iconeo.frm>
2024-08-25 12:00:42 +0200

    Changemetn photos home

--- index.php
+++ index.php
@@ -43,8 +43,10 @@
 				<!-- Slider -->
 				<div class="swiper-container">
 					<div class="swiper-wrapper">
-						<div class="swiper-slide" style="background-image:url('img/loriane2.jpg');"></div>
-						<div class="swiper-slide" style="background-image:url('img/delphine3.jpg');"></div>
+						<div class="swiper-slide" style="background-image:url('img/piercing1.jpg');"></div>
+						<div class="swiper-slide" style="background-image:url('img/piercing2.jpg');"></div>
+                        <div class="swiper-slide" style="background-image:url('img/piercing3.jpg');"></div>
+                        <div class="swiper-slide" style="background-image:url('img/piercing4.jpg');"></div>
 					</div>
 					<!-- Add Pagination -->
 					<div class="swiper-pagination"></div>
